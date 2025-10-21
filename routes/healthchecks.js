2ac1aaa120b6e0fed8040ef0b0eceb25632c1c24
Jorge Paiva <Phaktumn@users.noreply.github.com>
2025-10-21 15:58:08 +0100

    better error handlling

--- routes/healthchecks.js
+++ routes/healthchecks.js
@@ -4,13 +4,12 @@ const Anchor = require("../models/Anchor");
 
 router.get("/", async (req, res) => {
   try {
-    console.log("Healthcheck route hit: GET /api/health"); // Log when the healthcheck route is accessed
     // Check mongo connection
     await Anchor.find();
     res.status(200).send("Mongo access OK");
   } catch (err) {
-    console.error("Healthcheck Mongo access error:", err.message); // More specific error logging
-    res.status(500).send("Mongo access NOT OK");
+    // More specific error logging
+    res.status(500).send("Mongo access NOT OK", err.message);
   }
 });
 
